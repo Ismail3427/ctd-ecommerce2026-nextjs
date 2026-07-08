15772ecc1a92879a042d8d65a0be649120fb7ebc
Ismail3427 <ismailurfy@gmail.com>
2026-07-08 17:34:29 -0500

    Create page.tsx

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,57 @@
+import { auth0 } from "@/lib/auth0";
+import axios from "axios";
+import { Metadata } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Orders",
+  description: "View and manage your orders",
+};
+
+const OrdersPage = async () => {
+  const session = await auth0.getSession();
+
+  const { data: orders } = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/orders/get`,
+    {
+      userId: session?.user.sub,
+    },
+  );
+
+  console.log(orders);
+
+  return (
+    <main className="flex flex-col gap-5 items-center justify-center p-5">
+      <h1 className="font-bold text-6xl">Orders</h1>
+      <ul className="flex flex-col gap-5 w-[50vw]">
+        {orders.map((order: any) => (
+          <li
+            key={order.id}
+            className="p-5 w-full flex flex-col gap-3"
+          >
+            <p>Order ID: {order.id}</p>
+            <p>Status: {order.status}</p>
+            <p>Total: ${(order.totalInCents / 100).toFixed(2)}</p>
+            <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+            <h3 className="font-bold text-xl mt-3">Items:</h3>
+            <ul className="flex flex-col gap-3 pl-5">
+              {order.cart.map((item: any) => (
+                <li key={item.id} className="flex flex-col gap-2">
+                  <p className="font-bold text-xl">{item.product.name}</p>
+                  <Image
+                    src="/globe.svg"
+                    alt={item.product.name}
+                    width={100}
+                    height={100}
+                  />
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default OrdersPage;
